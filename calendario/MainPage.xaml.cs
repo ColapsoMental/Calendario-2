9242d49feceac9b308759d80831d3aafee62573e
UwU <jag.vasquez@yavirac.edu.ec>
2021-03-30 13:43:01 -0500

    Todavía no funciona

---
+++ calendario/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using DevExpress.XamarinForms.Scheduler;
+
+
+namespace calendario
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            DevExpress.XamarinForms.Scheduler.Initializer.Init();
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ calendario/ReceptionDeskViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Scheduler_GettingStarted
+{
+    public class ReceptionDeskViewModel : INotifyPropertyChanged
+    {
+        readonly ReceptionDeskData data;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DateTime StartDate { get { return ReceptionDeskData.BaseDate; } }
+        public IReadOnlyList<MedicalAppointment> MedicalAppointments
+        { get => data.MedicalAppointments; }
+
+        public ReceptionDeskViewModel()
+        {
+            data = new ReceptionDeskData();
+        }
+
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
